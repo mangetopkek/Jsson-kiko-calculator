fe10b2372a8bdf6a986ecb0f926b6995f6c13bc8
mangetopkek <j3473644@trbvm.com>
2016-01-31 22:55:25 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,5 +1,49 @@
+
+function button1(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"1";
+}
+function button2(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"2";
+}
+function button3(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"3";
+}
+function button4(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"4";
+}
+function button5(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"5";
+}
+function button6(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"6";
+}
+function button7(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"7";
+}
+function button8(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"8";
+}
+function button9(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"9";
+}
+function button0(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"0";
+}
+function buttondiv(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"/";
+}
+function buttonmulti(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"*";
+	
+}
+function buttonadd(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"+";
+	
+}
+function buttonsub(){
+	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+"-";
+}
 function input() {
-	var inputform = document.getElementById("fr1");
-	var input=inputform.elements['ruta'].value
+	var input = document.getElementById("fr1").elements['ruta'].value
 	document.getElementById("test").innerHTML = input;
 }
