ad22f061775682e4a31fe26c100ce481431b55df
mangetopkek <j3473644@trbvm.com>
2016-02-01 17:10:16 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,23 +1,22 @@
-
 function button(x){
 	document.getElementById("fr1").elements['ruta'].value=document.getElementById("fr1").elements['ruta'].value+x;
 }
 function clean(){
 	document.getElementById("fr1").elements['ruta'].value='';
 }
-/*function calculation() {
+function calculation() {
 	var i;
-	var input = document.getElementById("fr1").elements['ruta'].value
+	var input = document.getElementById("fr1").elements['ruta'].value;
 	var tal1,tal2,sum;
 	for (i = 0; i < input.length; i++){
-		if (input.charAt(i)=="+"){
+		if (input.charAt(i)=="+" ){
 			tal1 = input.substring(0,i);
 			tal2 = input.substring(i+1);
 		}
-		tal1=tal1.parseInt(tal1);
-		tal2=tal2.parseInt(tal2);
+		tal1=parseFloat(tal1);
+		tal2=parseFloat(tal2);
 		sum=tal1+tal2;
-		document.getElementById("test").innerHTML = sum;
-		
+		document.getElementById("fr1").elements['ruta'].value=sum;
+			
 	}
-}*/
+}
