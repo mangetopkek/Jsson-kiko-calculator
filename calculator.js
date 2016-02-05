9474e8d58d2a7e9f8da6a988698fb895b3cbc5d9
mangetopkek <j3473644@trbvm.com>
2016-02-05 12:50:38 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,19 +4,28 @@ function button(x){
 function clean(){
 	document.getElementById("fr1").elements['ruta'].value='';
 }
-function calculation() {
-	var i;
-	var input = document.getElementById("fr1").elements['ruta'].value;
-	var tal1,tal2,sum;
-	for (i = 0; i < input.length; i++){
-		if (input.charAt(i)=="+" ){
-			tal1 = input.substring(0,i);
-			tal2 = input.substring(i+1);
-		}
-		tal1=parseFloat(tal1);
-		tal2=parseFloat(tal2);
-		sum=tal1+tal2;
-		document.getElementById("fr1").elements['ruta'].value=sum;
-			
-	}
+function parser() {
+	var input=document.getElementById("fr1").elements['ruta'].value;
+        var tal=[];
+        var operatorer=[];
+        var length=input.length;
+        input=input.toString();
+        var start=0;
+        var sum=0;
+        
+        for (var i=0; i<length; i++){
+            if (input.charAt(i)==='+'){
+                tal.push(input.substring(start, i));
+                start=i+1;
+            }
+                
+        }
+        tal.push(input.substring(input.lastIndexOf("+")+1)) ;
+        var tal=tal.map(Number);
+     
+        for (i=0; i<tal.length; i++){
+            sum=sum+tal[i];
+        }
+        document.getElementById("fr1").elements['ruta'].value=sum;
 }
+
